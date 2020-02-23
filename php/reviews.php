b0cfdd2f23625a9c6a92927261a8b477383d24c2
Len <532904508@qq.com>
2020-02-24 04:08:11 +0800

    新增评论reviews.html动态化，修复了个人库存的bug

---
+++ php/reviews.php
@@ -0,0 +1,48 @@
+<?php
+  $success = array('msg' => 'OK');
+  //匹配数据部分
+  require_once("PDOsingleton.php");
+  $pdo = PDOsingleton::getPdo();
+  $page = 1;
+
+  $page = $_GET['page'];
+  $uid = $_GET['uid'];
+  $num = ($page-1)*8;
+  // sql
+  $sql = "select a.goodid,goodname,type,time,comment from goodcomment a inner join goodinfo b where a.goodid = b.goodid and userid = ".$uid." order by time desc limit ".$num.",8";
+  $result = $pdo -> prepare($sql);
+  $result -> execute();
+  $result -> bindColumn(1,$gid);
+  $result -> bindColumn(2,$gname);
+  $result -> bindColumn(3,$type);
+  $result -> bindColumn(4,$time);
+  $result -> bindColumn(5,$comment);
+  $info = [];
+  for($i=0;$result->fetch(PDO::FETCH_COLUMN);$i++){
+    $info[$i] = array('gid'=>$gid,'gname'=>$gname,'type'=>$type,'time'=>$time,'comment'=>$comment);
+  }
+  //将索引到的数据放入$success中并进行返回
+  $success['commentListInfo'] = $info;
+
+  //获取用户名字
+  $sql = 'select userName from userinfo where userid = '.$uid;
+  $result = $pdo -> prepare($sql);
+  $result -> execute();
+  $result -> bindColumn(1,$uname);
+  $result->fetch(PDO::FETCH_COLUMN);
+  $success['username'] = $uname;
+
+  // 获取总页数
+  $sql = 'select count(*) from goodcomment where userid = '.$uid;
+  $result = $pdo -> prepare($sql);
+  $result -> execute();
+  $result -> bindColumn(1,$sum);
+  $result->fetch(PDO::FETCH_COLUMN);
+  $sum /= 8;
+  $success['maxPage'] = ceil($sum);
+
+
+
+  echo json_encode($success);
+
+ ?>
